cd48d3a3bbc517e60fbe7c97f7b88052cf4b4aae
LucasDevSystem <highspeedsetup@gmail.com>
2023-06-23 08:41:38 -0300

    (App) Types , getting api data, request exceptios treatment

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,89 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { AxiosError } from "axios";
+
+import api from "./api";
+
+type Game = {
+  id: number;
+  title: string;
+  thumbnails: string;
+  short_description: string;
+  gamer_url: string;
+  genre: string;
+  platform: string;
+  publisher: string;
+  developer: string;
+  release_date: string;
+  freetogame_profrile_url: string;
+};
+type Errors = "TIMEOUT" | "SERVER_ERR" | "UNEXPECTED";
+
+const SERVER_ERRORS_5XX = [500, 502, 503, 504, 507, 508, 509];
 
 function App() {
+  const [loading, setLoading] = useState(false);
+  const [dataList, setDataList] = useState([]);
+
+  /**
+   * obtem o tipo de erro
+   * @param error erro emitido pelo axios
+   * @returns {Errors} tipo de erro str
+   */
+  function getRequestErrType(error: AxiosError): Errors {
+    const code = error?.code;
+    const status = error?.response?.status || 0;
+
+    // request timeout
+    if (code === "ECONNABORTED") return "TIMEOUT";
+    // server error
+    if (SERVER_ERRORS_5XX.includes(status)) return "SERVER_ERR";
+
+    return "UNEXPECTED";
+  }
+
+  /**
+   * Pelo tipo de erro identificado notifica o usuario
+   * @param errorType
+   */
+  function handleReqType(errorType: Errors) {
+    switch (errorType) {
+      // notifica usuario sobre o ocorrido
+      case "TIMEOUT":
+        break;
+      case "UNEXPECTED":
+        break;
+      // recarrega a pagina
+      case "SERVER_ERR":
+        break;
+
+      default:
+        break;
+    }
+  }
+
+  async function getData() {
+    setLoading(true);
+    try {
+      const { data = Array<Game> } = await api.get("/data/", {
+        headers: {
+          "dev-email-address": "teste@gmail.com",
+        },
+      });
+      console.log(data);
+    } catch (error: any) {
+      console.log(error);
+      handleReqType(getRequestErrType(error));
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
     </div>
   );
 }
