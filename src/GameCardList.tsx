eb05ed7248b633f836bc8751da4a80afb35bd963
LucasDevSystem <highspeedsetup@gmail.com>
2023-06-23 11:31:19 -0300

    feat (GameCardList) iterating over list of games

---
+++ src/GameCardList.tsx
@@ -0,0 +1,18 @@
+import { Grid } from "@mui/material";
+import GameCard from "./GameCard";
+
+// Lista dos cards dos jogos responsivas
+const GameCardList = ({ dataList = [] }) => {
+  return (
+    <div style={{ padding: 10, justifyContent: "center" }}>
+      <Grid container spacing={2}>
+        {dataList.map((item, index) => (
+          <Grid item xs={12} sm={6} md={4} key={index}>
+            <GameCard cardData={item}></GameCard>
+          </Grid>
+        ))}
+      </Grid>
+    </div>
+  );
+};
+export default GameCardList;
