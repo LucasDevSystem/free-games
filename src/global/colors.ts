0921dce4fe664e2346a2a8f7074179948c813e5b
LucasDevSystem <highspeedsetup@gmail.com>
2023-07-11 09:59:31 -0300

    Feat (colors) collors app palete

---
+++ src/global/colors.ts
@@ -0,0 +1,10 @@
+// paleta de cores da aplicacao
+// O adobe pick colors ajuda muito
+export const colors = {
+  background: "#141414",
+  card: "#222327",
+  green: "#27b4a4",
+  gray: "#222327",
+  ligthGray: "#b8bbbf",
+  white:"#edecec"
+};
