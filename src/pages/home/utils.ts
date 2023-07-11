2cd872c190ac7a359f5babdc07f491cf912369f6
LucasDevSystem <highspeedsetup@gmail.com>
2023-07-11 09:56:32 -0300

    Feat (utils): merge, getGenreOpts functions

---
+++ src/pages/home/utils.ts
@@ -0,0 +1,27 @@
+import { ApiGame, Favorite } from ".";
+
+// FUNCOES ULTILITARIAS
+
+export const mergeData = (dataList: ApiGame[], favorites: Favorite[]) => {
+  const gameListData = dataList.map((game: ApiGame) => {
+    const defaultFav = { stars: 0, isFavorite: false, gameId: 0 };
+    const favorite: Favorite =
+      favorites.find((fav: Favorite) => fav.gameId === game.id) || defaultFav;
+
+    return { ...game, stars: favorite.stars, isFavorite: favorite.isFavorite };
+  });
+
+  return gameListData;
+};
+
+export const getGenreOptions = (data: ApiGame[]) => {
+  let uniqueGenres: string[] = ["Todos"];
+
+  data.forEach((game: ApiGame) => {
+    if (!uniqueGenres.includes(game.genre)) {
+      uniqueGenres.push(game.genre);
+    }
+  });
+
+  return uniqueGenres;
+};
