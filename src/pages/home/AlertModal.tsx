0fa18559f6f5f4c8fd5c35ac3c4569824e607d49
LucasDevSystem <highspeedsetup@gmail.com>
2023-07-11 17:23:31 -0300

    Feat (AlertModal): basic modal to  ununtetnicaded users

---
+++ src/pages/home/AlertModal.tsx
@@ -0,0 +1,54 @@
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import { Box, Divider, Modal } from "@mui/material";
+
+export default function AlertModal({ isModalOpen, onClose }: any) {
+  return (
+    <Modal open={isModalOpen} onClose={onClose}>
+      <Box
+        sx={{
+          position: "absolute" as "absolute",
+          top: "50%",
+          left: "50%",
+          transform: "translate(-50%, -50%)",
+          border: "2px solid #000",
+          width: 400,
+        }}
+      >
+        <Card sx={{ minWidth: 275, backgroundColor: "#222327" }}>
+          <CardContent>
+            <Typography variant="h5" component="div">
+              Ops!! Login Nescessario
+            </Typography>
+            <Divider sx={{ margin: 2 }}></Divider>
+            <Typography variant="body1">
+              Entre na sua conta para continuar. Para uma melhor experiência,
+              precisamos que esteja logado.
+            </Typography>
+          </CardContent>
+          <CardActions sx={{ justifyContent: "flex-end" }}>
+            <Button
+              size="small"
+              sx={{ bgcolor: "gray", paddin: 5, margin: 1 }}
+              variant="contained"
+              onClick={onClose}
+            >
+              Fechar
+            </Button>
+            <Button
+              size="small"
+              sx={{ bgcolor: "#27b4a4" }}
+              href="/login"
+              variant="contained"
+            >
+              Fazer login
+            </Button>
+          </CardActions>
+        </Card>
+      </Box>
+    </Modal>
+  );
+}
