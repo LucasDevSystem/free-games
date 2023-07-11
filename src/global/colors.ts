22e0880e397e074b2cdaa3c4cf93099aaeb659d3
LucasDevSystem <highspeedsetup@gmail.com>
2023-07-11 17:15:58 -0300

    (colos) added new color

--- src/global/colors.ts
+++ src/global/colors.ts
@@ -3,8 +3,9 @@
 export const colors = {
   background: "#141414",
   card: "#222327",
+  lightCard: "#313238",
   green: "#27b4a4",
   gray: "#222327",
   ligthGray: "#b8bbbf",
-  white:"#edecec"
+  white: "#edecec",
 };
