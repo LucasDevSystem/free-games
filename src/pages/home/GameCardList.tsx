cfce5547045213c7a7d17f824887d317a24615b5
LucasDevSystem <highspeedsetup@gmail.com>
2023-07-14 16:00:40 -0300

    Fix (GameCardList): removed pagination

--- src/pages/home/GameCardList.tsx
+++ src/pages/home/GameCardList.tsx
@@ -1,9 +1,10 @@
 import { useState } from "react";
-import { Divider, Grid } from "@mui/material";
+import { Badge, Divider, Grid, Typography } from "@mui/material";
 
 import GameCard from "./GameCard";
 import FloatingPagination from "./FloatingPagination";
 import { Favorite, Game } from ".";
+import { colors } from "../../global/colors";
 
 const CARDS_PER_PAGE = 50;
 
@@ -20,12 +21,13 @@ const GameCardList = ({ dataList = [], onFavorite }: GameCardListProps) => {
   // faz a divisao e obtem o teto do valor
   const maxPages = Math.ceil(dataList.length / CARDS_PER_PAGE) || 1;
 
-  // dados visiveis da pagina
-  const cardsData = dataList.filter(
-    (game: Game, index: number) =>
-      index <= curentPage * CARDS_PER_PAGE &&
-      index >= (curentPage - 1) * CARDS_PER_PAGE
-  );
+  // // dados visiveis da pagina
+  // const cardsData = dataList.filter(
+  //   (game: Game, index: number) =>
+  //     index <= curentPage * CARDS_PER_PAGE &&
+  //     index >= (curentPage - 1) * CARDS_PER_PAGE
+  // );
+  const cardsData = dataList;
 
   function onFavoriteAdapter(isFavorite: boolean, game: Game) {
     return onFavorite({
@@ -42,24 +44,28 @@ const GameCardList = ({ dataList = [], onFavorite }: GameCardListProps) => {
       gameId: game.id,
     });
   }
-
+  if (!dataList.length) return null;
+  
   return (
     <div
       style={{
-        paddingTop: 8,
         justifyContent: "center",
       }}
     >
-      <Divider
-        sx={{
-          width: "80%",
-          margin: "auto",
-          paddingBottom: 3,
-        }}
-      >
-        {`${dataList.length} resultados`}
-      </Divider>
-      <Grid container spacing={4}>
+      <div style={{ marginBottom: 30 }}>
+        <Badge badgeContent={dataList.length} max={999} color="success">
+          <Typography variant="h4" fontWeight={"bold"}>
+            Jogos
+          </Typography>
+        </Badge>
+        <Divider
+          sx={{
+            width: "100%",
+            color: colors.darkGreen,
+          }}
+        />
+      </div>
+      <Grid container spacing={6}>
         {cardsData.map((item: any, index: number) => (
           <Grid item xs={12} sm={6} md={4} key={item.id}>
             <GameCard
@@ -70,10 +76,10 @@ const GameCardList = ({ dataList = [], onFavorite }: GameCardListProps) => {
           </Grid>
         ))}
       </Grid>
-      <FloatingPagination
+      {/* <FloatingPagination
         maxPages={maxPages}
         onChagePage={(nexPage: number) => setCurentPage(nexPage)}
-      />
+      /> */}
     </div>
   );
 };
